3d149ed5fe16981d814622140dfd573c38773816
benaka-tech <cjayanth35@gmail.com>
2020-09-11 05:06:05 +0530

    add http and Events

---
+++ console_learn.js
@@ -0,0 +1,8 @@
+console.info("Program Started");
+
+var counter=10;
+console.log("Counter: %d",counter);
+console.time("Getting Data");
+//do some processing here...
+console.timeEnd("Getting Data");
+console.info("Program Ended");
